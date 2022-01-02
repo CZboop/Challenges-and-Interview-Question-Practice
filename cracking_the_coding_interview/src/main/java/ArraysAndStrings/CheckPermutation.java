db2ccf7254904b414f8e51ef076a79430e133989
Branchey <70140890+CZboop@users.noreply.github.com>
2022-01-02 13:29:17 +0000

    added alternate method for check permutations, that doesn't use streams and may be more readable, also added tests for this testing in the same way as previous solution

--- cracking_the_coding_interview/src/main/java/ArraysAndStrings/CheckPermutation.java
+++ cracking_the_coding_interview/src/main/java/ArraysAndStrings/CheckPermutation.java
@@ -6,10 +6,7 @@ package ArraysAndStrings;
 
 //assuming exactly the same will not count as permutation and must have the same number of each character
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -19,6 +16,11 @@ public class CheckPermutation {
         System.out.println(check("abbbbdc", "bbadc"));
         System.out.println(check("dabbdc", "bbdfffadc"));
         System.out.println(check("abbdc", "abbdc"));
+
+        System.out.println(checkNoStream("abbdc", "bbadc"));
+        System.out.println(checkNoStream("abbbbdc", "bbadc"));
+        System.out.println(checkNoStream("dabbdc", "bbdfffadc"));
+        System.out.println(checkNoStream("abbdc", "abbdc"));
 
     }
     public static boolean check(String string1, String string2){
@@ -40,6 +42,34 @@ public class CheckPermutation {
                 .flatMap(a -> a.chars().mapToObj(c -> (char) c))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+        return string1Occurrences.equals(string2Occurrences);
+    }
+
+    public static boolean checkNoStream(String string1, String string2){
+        if (string1.equals(string2) || string1.length() != string2.length()){
+            return false;
+        }
+        if (string1.length() <= 1){
+            return false;
+        }
+        HashMap<String, Integer> string1Occurrences = new HashMap<>();
+        HashMap<String, Integer> string2Occurrences = new HashMap<>();
+        for (int i=0; i<string2.length(); i++){
+            String string1Char = String.valueOf(string1.charAt(i));
+            String string2Char = String.valueOf(string2.charAt(i));
+            if (string1Occurrences.containsKey(string1Char)){
+                Integer currentCount = string1Occurrences.get(string1Char);
+                string1Occurrences.put(string1Char, ++currentCount);
+            }else{
+                string1Occurrences.put(string1Char, 1);
+            }
+            if (string2Occurrences.containsKey(string2Char)){
+                Integer currentCount = string2Occurrences.get(string2Char);
+                string2Occurrences.put(string2Char, ++currentCount);
+            }else{
+                string2Occurrences.put(string2Char, 1);
+            }
+        }
         return string1Occurrences.equals(string2Occurrences);
     }
 }
--- cracking_the_coding_interview/src/test/java/ArraysAndStrings/CheckPermutationTest.java
+++ cracking_the_coding_interview/src/test/java/ArraysAndStrings/CheckPermutationTest.java
@@ -236,5 +236,228 @@ public class CheckPermutationTest {
         assertThat(actual).isFalse();
     }
 
+// testing alternative method that does not use streams, using the same tests
 
+    @Test
+    void canCheckOneEmptyStringNoStream() {
+        // Given
+        String string1 = "";
+        String string2 = "aa";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckBothEmptyStringsNoStream() {
+        // Given
+        String string1 = "";
+        String string2 = "";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckOneLetterStringNoStream() {
+        // Given
+        String string1 = "aa";
+        String string2 = "a";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckBothOneLetterStringsNoStream() {
+        // Given
+        String string1 = "a";
+        String string2 = "a";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckExactMatchNoStream() {
+        // Given
+        String string1 = "abcdefghi";
+        String string2 = "abcdefghi";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckShortPermutationNoStream() {
+        // Given
+        String string1 = "gefhi";
+        String string2 = "hefgi";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void canCheckLongPermutationNoStream() {
+        // Given
+        String string1 = "qwerqazthhhhhhbyuiopaaasslkjhmnbvcgfdsazxcvbnm";
+        String string2 = "asdfhhhhhhbghjklmnbvcxzqwzaqcvbnmertyuiopaaass";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void canCheckShortNonPermutationNoStream() {
+        // Given
+        String string1 = "err";
+        String string2 = "wer";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckLongNonPermutationNoStream() {
+        // Given
+        String string1 = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
+        String string2 = "jjjjjjjjjjjjjjjjjjjijjjjjjjjjjjjjjjj";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckSameLettersDifferentQuantityNoStream() {
+        // Given
+        String string1 = "aaaabbb";
+        String string2 = "aaabbbb";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckStringPermutationOfDigitsNoStream() {
+        // Given
+        String string1 = "1234444445678900";
+        String string2 = "0044444412356789";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void canCheckStringOfDigitsNonPermutationNoStream() {
+        // Given
+        String string1 = "12345555678909876";
+        String string2 = "12345678890976532";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canCheckStringOfSpecialCharsPermutationNoStream() {
+        // Given
+        String string1 = "?????@!!!!;::**::::...,,";
+        String string2 = "?::@!;:??..,?!?*:*.!!::,";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void canCheckStringOfSpecialCharsNonPermutationNoStream() {
+        // Given
+        String string1 = "@@@@????!!!!::::;;;;;;****";
+        String string2 = "@@****@??@!::::;;;;;??!!!*";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+    //    add string capitalisation tests
+    @Test
+    void canCheckStringWithDifferentCasesIsNotPermutationNoStream() {
+        // Given
+        String string1 = "AaabbcdefGGH";
+        String string2 = "aAABBCDEFggH";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
+
+
+    @Test
+    void canCheckLongMixedStringPermutationNoStream() {
+        // Given
+        String string1 = "aewr1234876ujereigjEEERUngh????!!;;;***TREWebb765423wretBgretebh??:::,,,,,Wew";
+        String string2 = "a23gh???***utebh??:::,,je1etBgre,,,Wereigj5423wreETRE4876EE?!!;;;RUnWebb76wrw";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void canCheckLongMixedStringNonPermutationNoStream() {
+        // Given
+        String string1 = "aaaeeeennnHHHHWWWYY1112227778888****????!!!!!;;;;;::::EEEYYRhhhhggbvnfdhdgrtf";
+        String string2 = "aaa::EEE;;778nHgHHdYYRh;;::27hh11rWWYY***?gbvnfdh1??8eeeNN?!88*hg22e!!!!;grtf";
+
+        // When
+        boolean actual = underTest.checkNoStream(string1, string2);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
 }
